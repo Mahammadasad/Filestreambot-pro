2a3210142435d3abad5f6326ea371c5a1bb50a00
Mahammadasad <157095169+Mahammadasad@users.noreply.github.com>
2024-01-31 12:16:53 +0530

    Update vars.py
    edit

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -22,14 +22,14 @@ class Var(object):
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "6708682548").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'hstrem'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Hstremapp'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '101.188.67.134')) if not ON_HEROKU or getenv('FQDN', '101.188.67.134') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
